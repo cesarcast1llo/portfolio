11c934ad1fb7895600f9b00b6cb706ac79021c83
cesarcast1llo <cesarcast1llo@yahoo.com>
2020-02-27 19:27:42 -0800

    remove percentage bars, outdated apparently

--- src/components/MyWork/SkillSliders.js
+++ src/components/MyWork/SkillSliders.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import VisibilitySensor from 'react-visibility-sensor';
-import 'react-circular-progressbar/dist/styles.css';
 import ScrollAnimation from 'react-animate-on-scroll';
 // shorten
 import github from '../../assets/images/logos/github.png';
@@ -13,6 +10,17 @@ import psd from '../../assets/images/logos/photoshop.png';
 import inD from '../../assets/images/logos/indesign.png';
 import xd from '../../assets/images/logos/xd.png';
 import sketch from '../../assets/images/logos/sketch.svg';
+
+import html from '../../assets/images/logos/html.png';
+import css from '../../assets/images/logos/css3.png';
+import javascript from '../../assets/images/logos/javascript.png';
+import react from '../../assets/images/logos/react.png';
+import emails from '../../assets/images/logos/htmlemail.svg';
+import next from '../../assets/images/logos/nextjs.png';
+import node from '../../assets/images/logos/nodejs.png';
+import gatsby from '../../assets/images/logos/gatsby.png';
+import wordpress from '../../assets/images/logos/wordpress.png';
+
 // find a way to shorten this down ^^^
 
 export default class SkillSliders extends Component {
@@ -21,14 +29,25 @@ export default class SkillSliders extends Component {
 
     this.state = {
       skills: [
-        { name: 'HTML', number: 90 },
-        { name: '(S)CSS', number: 90 },
-        { name: 'Javascript', number: 85 },
-        { name: 'React.js', number: 80 },
-        { name: 'Emails', number: 90 },
-        { name: 'Next.js', number: 80 },
-        { name: 'Node.js', number: 75 },
-        { name: 'Wordpress', number: 80 }
+        { name: 'HTML', img: html },
+        { name: '(S)CSS', img: css },
+        { name: 'Javascript', img: javascript },
+        { name: 'React.js', img: react },
+        { name: 'Emails', img: emails },
+        { name: 'Next.js', img: next },
+        { name: 'Node.js', img: node },
+        { name: 'Gatsby', img: gatsby },
+        { name: 'Wordpress', img: wordpress }
+      ],
+      tools: [
+        { name: 'Github', src: github },
+        { name: 'Heroku', src: heroku },
+        { name: 'AWS', src: aws },
+        { name: 'Salesforce', src: sf },
+        { name: 'Photoshop', src: psd },
+        { name: 'InDesign', src: inD },
+        { name: 'XD', src: xd },
+        { name: 'Sketch', src: sketch }
       ],
       itemsToShow: 4,
       expanded: false
@@ -54,7 +73,10 @@ export default class SkillSliders extends Component {
               delay={250}
               animateOnce={true}
             >
-              <div className="intro">What I can do.</div>
+              <div className="intro">
+                What I <br className="em_hide_mobile" />
+                Work&nbsp;With.
+              </div>
             </ScrollAnimation>
 
             <ScrollAnimation
@@ -67,25 +89,15 @@ export default class SkillSliders extends Component {
                 {this.state.skills
                   .slice(0, this.state.itemsToShow)
                   .map((skill, i) => (
-                    <Col xs="12" sm="6" md="3" className="skillName" key={i}>
-                      <VisibilitySensor intervalDelay={2500}>
-                        {({ isVisible }) => {
-                          return (
-                            <>
-                              <p>{skill.name}</p>
-                              <CircularProgressbar
-                                duration={3}
-                                counterClockwise
-                                value={isVisible ? skill.number : 0}
-                                text={`${skill.number}%`}
-                                styles={buildStyles({
-                                  pathTransitionDuration: 2.5
-                                })}
-                              />
-                            </>
-                          );
-                        }}
-                      </VisibilitySensor>
+                    <Col xs="6" sm="3" md="3" className="skillName" key={i}>
+                      <>
+                        <p>{skill.name}</p>
+                        <img
+                          alt={skill.name}
+                          className={skill.name}
+                          src={skill.img}
+                        />
+                      </>
                     </Col>
                   ))}
 
@@ -103,23 +115,33 @@ export default class SkillSliders extends Component {
             <ScrollAnimation
               animateIn="flipInX"
               duration={1}
-              delay={1000}
+              delay={750}
               animateOnce={true}
             >
               <Row>
                 <Col md="5" className="other-tools">
-                  Other Tools:
+                  Other Development&nbsp;Tools:
                 </Col>
                 <Col md="7" className="other-tools-icon">
-                  <img alt="Adobe Photoshop" className="psd" src={psd} />
-                  <img alt="Adobe InDesign" className="inD" src={inD} />
-                  <img alt="Sketch" className="sketch" src={sketch} />
-                  <img alt="Adobe XD" className="xd" src={xd} />
-                  <br className="em_hide_mobile" />
-                  <img alt="Github" className="github" src={github} />
-                  <img alt="Heroku" className="heroku" src={heroku} />
-                  <img alt="Amazon Web Services" className="aws" src={aws} />
-                  <img alt="Salesforce" className="sf" src={sf} />
+                  {this.state.tools.map((tool, i) => {
+                    return i === 3 ? (
+                      <div key={i} style={{ display: 'inline' }}>
+                        <img
+                          alt={tool.name}
+                          className={tool.name}
+                          src={tool.src}
+                        />
+                        <br className="em_hide_mobile" />
+                      </div>
+                    ) : (
+                      <img
+                        key={i}
+                        alt={tool.name}
+                        className={tool.name}
+                        src={tool.src}
+                      />
+                    );
+                  })}
                 </Col>
               </Row>
             </ScrollAnimation>
@@ -129,3 +151,18 @@ export default class SkillSliders extends Component {
     );
   }
 }
+// {tags.map(function(tag, index) {
+//   return index % 2 === 0 ? (
+//     <Link key={tag} to={`/tags/${slugify(tag)}`}>
+//       <Button color="primary" className="tag-button">
+//         {tag} <div className="number">{tagPostCounts[tag]}</div>
+//       </Button>
+//     </Link>
+//   ) : (
+//     <Link key={tag} to={`/tags/${slugify(tag)}`}>
+//       <Button color="primary" className="odd">
+//         {tag} <div className="number">{tagPostCounts[tag]}</div>
+//       </Button>
+//     </Link>
+//   );
+// })}

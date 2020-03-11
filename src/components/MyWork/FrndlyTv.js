e0f55718694dc9e4c2e23dc2e1e66a7e2de6b5e3
cesarcast1llo <cesarcast1llo@yahoo.com>
2020-03-11 16:52:40 -0700

    project section finished, moving on to get this live

---
+++ src/components/MyWork/FrndlyTv.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
+import { Row, Col } from 'reactstrap';
+import javascript from '../../assets/images/logos/javascript.png';
+import next from '../../assets/images/logos/nextjs.png';
+import react from '../../assets/images/logos/react.png';
+import node from '../../assets/images/logos/nodejs.png';
+import frndly from '../../assets/images/frndly.jpg';
+
+// import ScrollAnimation from 'react-animate-on-scroll';
+
+export default class FrndlyTv extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Frndly TV',
+      img: frndly,
+      stack: [
+        { name: 'Node.js', img: node, number: 350 },
+        { name: 'Next.js', img: next, number: 450 },
+        { name: 'Javascript', img: javascript, number: 550 },
+        { name: 'React.js', img: react, number: 650 }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <ScrollAnimation
+        animateIn="fadeInLeft"
+        duration={1}
+        delay={250}
+        animateOnce={true}
+      >
+        <div className="project-layout">
+          <Row>
+            <Col md={{ size: 6 }} className="project-description">
+              <img alt="Frndly Tv" className="project-logo" src={frndly} />
+              <h1>Frndly</h1>
+              <p>
+                Lorem ipsuma asfa asdasde asdasd asdasdbu ipsuma asfa asdasde
+                asdasd asdasdbu ipsuma asfa asdasde asdasd asdasdbu ipsuma asfa
+                asdasde asdasd asdasdbu
+              </p>
+              <h2>What I used:</h2>
+
+              {this.state.stack.map((stack, i) => (
+                <div className="logos">
+                  <ScrollAnimation
+                    animateIn="fadeIn"
+                    duration={2.5}
+                    animateOnce={true}
+                    key={i}
+                  >
+                    <img alt={stack.name} src={stack.img} />
+                  </ScrollAnimation>
+                </div>
+              ))}
+            </Col>
+            <Col md={{ size: 6, order: 1 }} className="project-images">
+              <img alt="Frndly Tv" src={frndly} />
+              <img alt="Frndly Tv" src={frndly} />
+            </Col>
+          </Row>
+        </div>
+      </ScrollAnimation>
+    );
+  }
+}
--- src/components/MyWork/Projects.js
+++ src/components/MyWork/Projects.js
@@ -1,60 +1,60 @@
-import React, { Component } from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
+import React from 'react';
 import { Row, Col } from 'reactstrap';
-import javascript from '../../assets/images/logos/javascript.png';
-import next from '../../assets/images/logos/nextjs.png';
-import react from '../../assets/images/logos/react.png';
-import node from '../../assets/images/logos/nodejs.png';
-import frndly from '../../assets/images/frndly.jpg';
+import FrndlyTv from './FrndlyTv';
+import LiftSite from './LiftSite';
+import HTMLEmails from './HTMLEmails';
+import OtherProjects from './OtherProjects';
+import ScrollAnimation from 'react-animate-on-scroll';
 
-// import ScrollAnimation from 'react-animate-on-scroll';
+const Projects = () => {
+  return (
+    <>
+      <Row>
+        <Col className="header">
+          <ScrollAnimation
+            animateIn="fadeIn"
+            duration={1}
+            delay={250}
+            animateOnce={true}
+          >
+            <div className="header">
+              {/* Projects I've <br className="em_hide_mobile" />
+              Worked&nbsp;On */}
+              Lorem Ipsum
+            </div>
+          </ScrollAnimation>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="projects">
+          <div className="format">
+            <FrndlyTv />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="odd">
+          <div className="format">
+            <LiftSite />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="projects">
+          <div className="format">
+            <HTMLEmails />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="odd">
+          <div className="format">
+            <OtherProjects />
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default class Projects extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'Frndly TV',
-      img: frndly,
-      stack: [
-        { name: 'Node.js', img: node, number: 350 },
-        { name: 'Next.js', img: next, number: 450 },
-        { name: 'Javascript', img: javascript, number: 550 },
-        { name: 'React.js', img: react, number: 650 }
-      ]
-    };
-  }
-
-  render() {
-    return (
-      <div className="project-layout">
-        <Row>
-          <Col sm={{ size: 6, order: 2 }} className="project-description">
-            <img alt="Frndly Tv" className="project-logo" src={frndly} />
-            <h1>astafs we auasd</h1>
-            <p>
-              Lorem ipsuma asfa asdasde asdasd asdasdbu ipsuma asfa asdasde
-              asdasd asdasdbu ipsuma asfa asdasde asdasd asdasdbu ipsuma asfa
-              asdasde asdasd asdasdbu
-            </p>
-
-            {this.state.stack.map((stack, i) => (
-              <ScrollAnimation
-                animateIn="fadeIn"
-                duration={2.5}
-                animateOnce={true}
-                key={i}
-              >
-                <img alt={stack.name} className="logos" src={stack.img} />
-              </ScrollAnimation>
-            ))}
-          </Col>
-          <Col sm={{ size: 6, order: 1 }} className="project-images">
-            <img alt="Frndly Tv" src={frndly} />
-            <p>this one below mobile hide</p>
-            <img alt="Frndly Tv" src={frndly} />
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+export default Projects;

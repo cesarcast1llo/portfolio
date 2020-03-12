6d509abb650de81194271aeb5b110a179387da79
cesarcast1llo <cesarcast1llo@yahoo.com>
2020-03-12 12:35:08 -0700

    netlify fix

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import SkillSliders from '../components/myWork/SkillSliders';
-import Projects from '../components/myWork/Projects';
+import SkillSliders from './myWork/SkillSliders';
+import Projects from './myWork/Projects';
 import Contact from './myWork/Contact';
 import ScrollAnimation from 'react-animate-on-scroll';
 // import Data from '../pages/data.json';
